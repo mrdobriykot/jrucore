6a9412a48b12c7890e79c59dab01b2aec7338f04
mrdobriykot <evgkorin@yandex.ru>
2022-08-18 22:31:38 +0300

    add executor servise for multithreading

--- src/simulation/AnimalCreation.java
+++ src/simulation/AnimalCreation.java
@@ -1,2 +1,46 @@
-package simulation;public class AnimalCreation {
+package simulation;
+
+import creatures.animals.carnivoreanimals.*;
+import creatures.animals.herbivoreanimals.*;
+
+import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class AnimalCreation {
+    public static void creationOfAnimal() {
+        List <Runnable> listOfTask = new ArrayList<>();
+        listOfTask.add(new AnimalDeploy(Bear.class));
+        listOfTask.add(new AnimalDeploy(Eagle.class));
+        listOfTask.add(new AnimalDeploy(Fox.class));
+        listOfTask.add(new AnimalDeploy(Snake.class));
+        listOfTask.add(new AnimalDeploy(Wolf.class));
+        listOfTask.add(new AnimalDeploy(Boar.class));
+        listOfTask.add(new AnimalDeploy(Buffalo.class));
+        listOfTask.add(new AnimalDeploy(Caterpiller.class));
+        listOfTask.add(new AnimalDeploy(Deer.class));
+        listOfTask.add(new AnimalDeploy(Duck.class));
+        listOfTask.add(new AnimalDeploy(Goat.class));
+        listOfTask.add(new AnimalDeploy(Horse.class));
+        listOfTask.add(new AnimalDeploy(Mouse.class));
+        listOfTask.add(new AnimalDeploy(Rabbit.class));
+        listOfTask.add(new AnimalDeploy(Sheep.class));
+
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (Runnable task : listOfTask) {
+            System.out.println(executorService.submit(task));
+        }
+
+        executorService.shutdown();
+
+
+    }
+
+
+
+
 }
