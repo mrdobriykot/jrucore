a659c97b66d39c5f7f423cf6272518de32786e62
mrdobriykot <evgkorin@yandex.ru>
2022-08-20 12:43:24 +0300

    Experiments with eating class

---
+++ src/helper/EnumAnimals.java
@@ -0,0 +1,19 @@
+package helper;
+
+public enum EnumAnimals {
+    BEAR,
+    BOA,
+    EAGLE,
+    FOX,
+    WOLF,
+    BOAR,
+    BUFFALO,
+    CATERPILLAR,
+    DEER,
+    DUCK,
+    GOAT,
+    HORSE,
+    MOUSE,
+    RABBIT,
+    SHEEP
+}
